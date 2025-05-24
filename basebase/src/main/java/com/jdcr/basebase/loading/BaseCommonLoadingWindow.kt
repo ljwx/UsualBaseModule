ffa4f4a7e015da148901ba76dbf333d6781c86d2
jdcr <1146699396@qq.com>
2025-05-24 15:53:32 +0800

    [opt]:loading

--- basebase/src/main/java/com/jdcr/basebase/loading/BaseCommonLoadingWindow.kt
+++ basebase/src/main/java/com/jdcr/basebase/loading/BaseCommonLoadingWindow.kt
@@ -1,9 +1,11 @@
 package com.jdcr.basebase.loading
 
 import android.content.Context
+import android.graphics.Color
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.Window
 import android.widget.FrameLayout
@@ -11,11 +13,11 @@ import android.widget.PopupWindow
 import android.widget.ProgressBar
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.core.view.setPadding
 import androidx.core.graphics.drawable.toDrawable
 import com.jdcr.basebase.BaseModuleLog
 import com.jdcr.basebase.context.getRootView
+import com.jdcr.baseresource.R
 
 open class BaseCommonLoadingWindow(
     private val context: Context,
@@ -38,47 +40,102 @@ open class BaseCommonLoadingWindow(
     }
 
     private val defaultProgressbar by lazy {
-        val container = FrameLayout(context)
-        val progressBar = ProgressBar(context)
-        progressBar.setPadding(45)
-        val layoutParams = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        layoutParams.gravity = Gravity.CENTER
-        progressBar.layoutParams = layoutParams
+        val container = FrameLayout(context).apply {
+            setBackgroundResource(R.drawable.base_resource_shape_loading_background)
+            layoutParams = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        }
+        val progressBar = ProgressBar(context).apply {
+            setPadding(45)
+            layoutParams = FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                gravity = Gravity.CENTER
+            }
+        }
         container.addView(progressBar)
         container
     }
 
     private var windowType = 0x00
 
+    private fun getDefaultWidth(): Int {
+        return (context.resources.displayMetrics.widthPixels * 0.35).toInt()
+    }
+
     private val dialog by lazy {
         val view = loadingView ?: defaultProgressbar
         val dialog = AlertDialog.Builder(context).setView(view).create()
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window?.setDimAmount(0f)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.window?.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
+        if (loadingView == null) {
+            dialog.setOnShowListener {
+                dialog.window?.setLayout(getDefaultWidth(), WRAP_CONTENT)
+            }
+        }
         beforeCallback?.invoke(view)
-        windowType = windowType or 0x01
+        enableDialog()
         dialog
     }
 
     private val popupWindow by lazy {
         val view = loadingView ?: defaultProgressbar
         val popup = PopupWindow(view, WRAP_CONTENT, WRAP_CONTENT)
-        popup.setBackgroundDrawable(
-            ContextCompat.getColor(context, android.R.color.darker_gray).toDrawable()
-        )
+        popup.setBackgroundDrawable(Color.TRANSPARENT.toDrawable())
         beforeCallback?.invoke(view)
-        windowType = windowType or 0x10
+        enablePopupWindow()
         popup
+    }
+
+    private fun enableDialog() {
+        windowType = windowType or 0x01
+    }
+
+    private fun enablePopupWindow() {
+        windowType = windowType or 0x10
+    }
+
+    private fun isDialogActive(): Boolean {
+        return (windowType and 0x01) != 0
+    }
+
+    private fun isPopupActive(): Boolean {
+        return (windowType and 0x10) != 0
+    }
+
+    fun setDismissListener(dialogElsePop: Boolean = true, dismiss: () -> Unit) {
+        if (dialogElsePop) {
+            dialog.setOnDismissListener {
+                BaseModuleLog.dLoading("onDialogDismiss")
+                dismiss()
+            }
+        } else {
+            popupWindow.setOnDismissListener {
+                BaseModuleLog.dLoading("onPopupDismiss")
+                dismiss()
+            }
+        }
+    }
+
+    fun isShowing(dialogElsePop: Boolean = true): Boolean {
+        if (dialogElsePop && isDialogActive()) {
+            return dialog.isShowing
+        }
+        if (!dialogElsePop && isPopupActive()) {
+            return popupWindow.isShowing
+        }
+        return false
     }
 
     fun show(dialogElsePop: Boolean = true, cancelable: Boolean = true) {
         if (dialogElsePop) {
             if (!dialog.isShowing) {
+                BaseModuleLog.dLoading("显示dialog")
                 dialog.setCancelable(cancelable)
                 dialog.show()
             }
         } else {
             context.getRootView()?.let {
-                BaseModuleLog.dLoading("根布局不为空")
+                BaseModuleLog.dLoading("显示popup")
                 if (!popupWindow.isShowing) {
                     popupWindow.isFocusable = cancelable
                     popupWindow.showAtLocation(it, Gravity.CENTER, 0, 0)
@@ -88,10 +145,12 @@ open class BaseCommonLoadingWindow(
     }
 
     fun dismiss() {
-        if ((windowType and 0x10) != 0 && popupWindow.isShowing) {
+        if (isPopupActive() && popupWindow.isShowing) {
+            BaseModuleLog.dLoading("popup dismiss")
             popupWindow.dismiss()
         }
-        if ((windowType and 0x01) != 0 && dialog.isShowing) {
+        if (isDialogActive() && dialog.isShowing) {
+            BaseModuleLog.dLoading("dialog dismiss")
             dialog.dismiss()
         }
     }
